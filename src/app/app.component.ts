a51c59f1eb3e27a8392d853a83ae11fbbbaffed7
selrouvier <selrouvier@gmail.com>
2021-02-15 14:25:18 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PersonajesService } from './personajes.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'RICKYYMORTY';
+
+  arrPersonajes: any[];
+
+  currentPage: number;
+  numPages: number;
+
+  constructor( private PersonajesService: PersonajesService) {
+
+    this.currentPage = 1;
+  
+  }
+
+  ngOnInit() {
+    this.PersonajesService.getAll()
+      .then(response => {
+        this.arrPersonajes = response['results'];
+        this.numPages = response['info']['pages'];
+      })
+  }
+
+  async changePage(siguiente) {
+    if (siguiente) {
+      this.currentPage++;
+    } else {
+      this.currentPage--;
+    }
+    const response = await this.PersonajesService.getAll(this.currentPage);
+    this.arrPersonajes = response['results'];
+  }
+
+
 }
